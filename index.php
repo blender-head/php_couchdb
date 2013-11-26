c83255bf2f07b798839749f80ade23ba02d21342
andre.hartiawan <andre.hartiawan@wgs.co.id>
2013-11-26 13:44:43 +0700

    add new http route

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 		$app->render('home');
 	});
 
-	get('/signup', function($app) {
-		$app->render('signup');
+	post('/signup', function($app) {
+		$app->set('message', 'Thanks for Signing Up ' .
+		$app->form('name') . '!');
+		$app->render('home');
 	});
+
 
