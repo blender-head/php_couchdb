23a808913203b9f68669ccff20a47ff3536f09eb
andre.hartiawan <andre.hartiawan@wgs.co.id>
2013-11-26 14:16:50 +0700

    insert into couchdb document using php and curl

--- index.php
+++ index.php
@@ -7,11 +7,41 @@
 		$app->render('home');
 	});
 
+	get('/signup', function($app) {
+		$app->render('signup');
+	});
+
+
 	post('/signup', function($app) {
-		$app->set('message', 'Thanks for Signing Up ' .
-		$app->form('name') . '!');
+		$user = new stdClass;
+		$user->type = 'user';
+		$user->name = $app->form('name');
+		$user->email = $app->form('email');
+		echo json_encode($user);
+
+		$curl = curl_init();
+
+		// curl options
+		$options = array(
+			CURLOPT_URL => 'localhost:5984/verge',
+			CURLOPT_POSTFIELDS => json_encode($user),
+			CURLOPT_HTTPHEADER => array ("Content-Type: application/json"),
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "utf-8",
+			CURLOPT_HEADER => false,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_AUTOREFERER => true
+		);
+		
+		curl_setopt_array($curl, $options);
+		curl_exec($curl);
+		curl_close($curl);
+
+		$app->set('message', 'Thanks for Signing Up ' . $app->form('name') . '!');
 		$app->render('home');
 	});
+
 
 	get('/say/:message', function($app) {
 		$app->set('message', $app->request('message'));
