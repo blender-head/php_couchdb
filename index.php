f479c94c47b0f79749f0da262b2fa0be40ddda72
andre.hartiawan <andre.hartiawan@wgs.co.id>
2013-11-26 13:34:43 +0700

    add views rendering capability

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 	include 'lib/bones.php';
 
 	get('/', function($app) {
-		echo "Home";
+		$app->set('message', 'Welcome Back!');
+		$app->render('home');
 	});
 
-	get('/signup', function($app) 	{
-		echo "Signup!";
+	get('/signup', function($app) {
+		$app->render('signup');
 	});
+
--- lib/bones.php
+++ lib/bones.php
@@ -3,6 +3,8 @@
 	ini_set('display_errors','On');
 	error_reporting(E_ERROR | E_PARSE);
 
+	define('ROOT', __DIR__ . '/..');
+
 	function get($route, $callback) 
 	{
 		Bones::register($route, $callback);
@@ -14,6 +16,9 @@
 		private static $instance;
 		public static $route_found = false;
 		public $route = '';
+		public $content = '';
+		public $vars = array();
+
 
 		public function __construct() 
 		{
@@ -60,6 +65,27 @@
 			}
 		}
 
+		public function set($index, $value) {
+			$this->vars[$index] = $value;
+		}
 
+		public function render($view, $layout = "layout") 
+		{
+			$this->content = ROOT. '/views/' . $view . '.php';
+		
+			foreach ($this->vars as $key => $value) 
+			{
+				$$key = $value;
+			}
+		
+			if (!$layout) 
+			{
+				include($this->content);
+			} 
+			else 
+			{
+				include(ROOT . '/views/' . $layout . '.php');
+			}
+		}
 
 	}
